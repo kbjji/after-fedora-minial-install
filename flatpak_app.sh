2f336245359e6e5ff46216a83002fd13e74c7360
kbjji <58936443+kbjji@users.noreply.github.com>
2020-01-08 10:32:16 +0900

    Update flatpak_app.sh

--- flatpak_app.sh
+++ flatpak_app.sh
@@ -1,45 +1,17 @@
 #!/bin/sh
 
-COMMAND_NAME=`/usr/bin/echo $0 | /usr/bin/awk -F '/' '{print $NF}'`
-APP_BIN_PATH="${HOME}/flatpak-bin"
+APP_BIN_PATH="${HOME}/Apps/bin"
+cd ${APP_BIN_PATH}
 
-if [[ ${COMMAND_NAME} == "flatpak_app.sh" ]]
-then
-	if [[ z$1 == "zupdate" ]]
-	then
-		cd ${APP_BIN_PATH}
-		LINK_LIST=`/usr/bin/ls -l | /usr/bin/grep ^l | /usr/bin/grep flatpak_app.sh | /usr/bin/awk '{print $9}'`
-		for UNLINK_EXE in ${LINK_LIST}
-		do
-			/usr/bin/unlink ${UNLINK_EXE}
-		done
-		FLATPAK_APP_LIST=`/usr/bin/flatpak list --app --columns=application | /usr/bin/grep -v 'Application ID' | \
-			/usr/bin/awk -F '.' '{ if ( $NF == "Client" || $NF == "Application" || $NF == "Studio" ) print $(NF-1); else print $NF; }' | /usr/bin/tr "[A-Z]" "[a-z]"`
-		for APP in ${FLATPAK_APP_LIST}
-		do
-			/usr/bin/ln -s `whereis flatpak_app.sh | awk '{print $2}'` ${APP}
-		done
-		exit 0
-	else
-		APP_NAME=$1
-		if [[ z${APP_NAME} == "z" ]]
-		then
-			echo "USE: flatpak_app.sh [Application ID] || update"
-			exit 0
-		fi
-	fi
-else
-	APP_NAME=${COMMAND_NAME}
-fi
+LINK_LIST=`/usr/bin/ls -l | /usr/bin/grep ^l | /usr/bin/grep '/var/lib/flatpak/exports/bin/' | /usr/bin/awk '{print $9}'`
+for UNLINK_EXE in ${LINK_LIST}
+do
+	/usr/bin/unlink ${UNLINK_EXE}
+done
 
-if [[ z${APP_NAME} != "z" ]]
-then			
-	APP_ID=`/usr/bin/flatpak list --app --columns=application | /usr/bin/grep -i ${APP_NAME}`
-	if [[ z${APP_ID} == "z" ]]
-	then
-		echo "ERROR: ${APP_NAME} seems to have been uninstalled"
-		echo "       Please unlink ${APP_NAME} file"
-		exit 1
-	fi
-	/usr/bin/flatpak run ${APP_ID} $@
-fi
+FLATPAK_APP_LIST=`/usr/bin/flatpak list --app --columns=application | /usr/bin/grep -v 'Application ID'`
+for APP in ${FLATPAK_APP_LIST}
+do
+	APP_COMMAND=`/usr/bin/flatpak info -m ${APP} | /usr/bin/grep "^command" | /usr/bin/awk -F '=' '{print $2}'`
+	/usr/bin/ln -s /var/lib/flatpak/exports/bin/${APP} ${APP_COMMAND}
+done
