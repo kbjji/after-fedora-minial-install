399cf9ffc661527ca4a0b5b9987521e5ba000844
kbjji <58936443+kbjji@users.noreply.github.com>
2020-05-03 13:34:52 +0900

    Update other_desktop_environments_installer.sh

--- other_desktop_environments_installer.sh
+++ other_desktop_environments_installer.sh
@@ -15,10 +15,10 @@ echo -n "select(10s): "
 read -t 10 TARGET
 case $TARGET in
 	1|kde) ${CMD} plasma-nm plasma-desktop dolphin ;;
-	2|mate) ${CMD} network-manager-applet imsettings-mate mate-themes mate-desktop gtk2-engines marco caja ;;
-	3|xfce) ${CMD} network-manager-applet xfce4-session xfce4-panel xfce4-settings xfwm4 xfdesktop fedora-icon-theme ;;
-	4|cinnamon) ${CMD} network-manager-applet imsettings-cinnamon cinnamon nemo ;;
+	2|mate) ${CMD} imsettings-mate mate-themes mate-desktop gtk2-engines marco caja ;;
+	3|xfce) ${CMD} xfce4-session xfce4-panel xfce4-settings xfwm4 xfdesktop fedora-icon-theme ;;
+	4|cinnamon) ${CMD} imsettings-cinnamon cinnamon nemo ;;
 	5|deepin) ${CMD} deepin-desktop deepin-file-manager ;;
-	6|lxde) ${CMD} network-manager-applet lxde-common pcmanfm lxappearance-obconf fedora-icon-theme ;;
+	6|lxde) ${CMD} lxde-common pcmanfm lxappearance-obconf fedora-icon-theme ;;
 	*) echo "Do Not thing"; exit 0 ;;
 esac
